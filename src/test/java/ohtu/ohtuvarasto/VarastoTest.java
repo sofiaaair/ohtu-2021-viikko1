f1827a1a82cccba12fb806037a4ef996c1e8ab8b
sofiaaair <sofia.airas@hotmail.com>
2021-11-20 23:47:30 +0200

    JUnit

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -64,5 +64,84 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+    
+    @Test
+    public void liikaOttaminenPalauttaaOikeanMaaran() {
+        varasto.lisaaVarastoon(5);
+        
+        double saatuMaara = varasto.otaVarastosta(8);
+        
+        assertEquals(5, saatuMaara, vertailuTarkkuus);
+    }
+    
+    @Test
+    public void liikaLaittaminenMeneeHukkaan() {
+        varasto.lisaaVarastoon(5);
+        varasto.lisaaVarastoon(8);
+        
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void negatiivinenOttaminenPalauttaaOikein() {
+        varasto.lisaaVarastoon(8);
+        
+        double saatuMaara = varasto.otaVarastosta(-1);
+        
+        assertEquals(0, saatuMaara, vertailuTarkkuus);
+    }
+    
+    @Test
+    public void negatiivinenLisaaminenLisaaNolla() {
+        varasto.lisaaVarastoon(-5);
+        
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void negatiivinenTilavuusMuuttuuNollaksi() {
+        Varasto nollavarasto = new Varasto(-1);
+        
+        assertEquals(0, nollavarasto.getTilavuus(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void varastoAlkusaldollaPalauttaaOikein() {
+        Varasto alkusaldovarasto = new Varasto(10, 5);
+        
+        assertEquals(10, alkusaldovarasto.getTilavuus(), vertailuTarkkuus);
+    }
 
+    @Test
+    public void negatiivinenAlkusaldovarastonTilavuusOnNolla() {
+        Varasto alkusaldovarasto = new Varasto(-1, -10);
+        
+        assertEquals(0, alkusaldovarasto.getTilavuus(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void negatiivinenAlkusaldovarastonSaldoOnNolla() {
+        Varasto alkusaldovarasto = new Varasto(10, -5);
+        
+        assertEquals(0, alkusaldovarasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void liikaAlkusaldoMeneeHukkaan() {
+        Varasto alkusaldovarasto = new Varasto(10, 15);
+        
+        assertEquals(10, alkusaldovarasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    
+    public void toStringPalauttaaOikein() {
+        varasto.lisaaVarastoon(1);
+        String teksti = varasto.toString();
+        
+        assertEquals("saldo = 1.0, vielä tilaa 9.0", teksti);
+    }
+    
+    
+    
 }
